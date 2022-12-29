7164eb346d249598550dfb944e11e6dda1f3af29
Stefan Zweifel <hello@stefanzweifel.io>
2022-12-29 11:58:59 +0100

    Add gt alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,6 +41,7 @@ alias gs="git status"
 alias gc="git checkout"
 alias gpo="git push origin"
 alias gm="git merge"
+alias gt="git trim --merged"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias nah='git reset --hard;git clean -df'
 
